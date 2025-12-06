8a22b10c208745073aa8bf5f3560adae9d2cc901
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-06 00:55:26 +0000

    fix syntax

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except:
-        pass
+    except Exception:
+        result = None
     finally:
         print("Inside result: {}".format(result))
     return result
