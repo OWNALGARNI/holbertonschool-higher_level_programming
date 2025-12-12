d6449870082e335666e341352b99ace447bddd82
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-12 23:10:28 +0000

    add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square with a given size."""
+        self.__size = size
