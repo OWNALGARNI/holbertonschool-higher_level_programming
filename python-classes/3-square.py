5a0d704069740b5e0a39e0e7aa606c81c0ebebef
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-12 23:46:58 +0000

    Requirement fail

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,11 +6,7 @@ class Square:
     """This class defines a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square with validated size.
-
-        Args:
-            size (int): The size of the square.
-        """
+        """Initialize a new Square with validated size."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
