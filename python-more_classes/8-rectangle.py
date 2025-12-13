bd0439cabef3d7ce5f0e03c96394261995fa332e
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-13 15:08:13 +0000

    add 8-rectangle.py

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class with a comparison static method."""
+
+
+class Rectangle:
+    """Defines a rectangle with private width and height attributes."""
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+
+        Args:
+            value (int): The width value.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
