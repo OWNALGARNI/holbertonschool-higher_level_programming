15f915c203eed6971b75e70e648d6dc0fc99b89a
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-20 17:09:49 +0300

    Fix import to task_01_duck_typing

--- python-abc/main_01_shapes.py
+++ python-abc/main_01_shapes.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-from task_01_shapes import Circle, Rectangle, shape_info
+from task_01_duck_typing import Circle, Rectangle, shape_info
 
 c = Circle(5)
 r = Rectangle(4, 6)
 
 shape_info(c)
 shape_info(r)
+
