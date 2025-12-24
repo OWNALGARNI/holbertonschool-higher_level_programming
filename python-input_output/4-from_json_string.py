d50093252349af3f3231634c3044feb9fb85934b
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-24 08:40:44 +0000

    add 4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that provides a function to convert a JSON string to a Python object."""
+"""Module that provides a function to convert a JSON string
+to a Python object.
+"""
 
 
 import json
