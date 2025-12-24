625c3bd2b3880c8a6fad508eedc7bf77b2ce2a4e
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-24 07:49:47 +0000

    add 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that provides a function to write text to a file."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file (UTF-8) and return the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
