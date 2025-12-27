f3fbce619b409eeb4ab4d288e223d934a854a450
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-27 09:58:08 +0000

    add 10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with filtered JSON serialization."""
+
+
+class Student:
+    """Class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes listed in attrs
+        are included in the returned dictionary.
+        Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        return self.__dict__
