43858e2dc873c67c5d3de7a9aaf716833c6255b9
OWNALGARNI <s.xooxs@hotmail.com>
2025-12-27 09:53:52 +0000

    fix output

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,6 +6,6 @@ description of an object for JSON serialization.
 
 def class_to_json(obj):
     """Return the dictionary description of an object
-    for JSON serialization.
-    """
-    return obj.__dict___
+for JSON serialization.
+"""
+    return obj.__dict__
