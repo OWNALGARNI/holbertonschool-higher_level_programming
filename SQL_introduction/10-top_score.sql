bb2e6c5b7905717c9f589d84c202cf020f86d1de
OWNALGARNI <s.xooxs@hotmail.com>
2026-01-17 00:08:13 +0000

    add 10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records from second_table ordered by score (highest first)
+-- Display only the score and name columns in this order
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
