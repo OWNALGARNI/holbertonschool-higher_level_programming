d40f39d4c71d404a87ef355c4f1ac24274dedfaf
OWNALGARNI <s.xooxs@hotmail.com>
2026-01-17 11:28:22 +0000

    add 1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create the MySQL user user_0d_1 if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases and tables to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Reload the privilege tables
+FLUSH PRIVILEGES;
